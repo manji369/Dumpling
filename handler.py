cab4f75ce21afa3d87e23eb7cfd69a4594d2da0d
manjit <mmanukon@asu.edu>
2018-01-23 10:53:56 -0700

    Added dynamodb integration and published to skill store

--- handler.py
+++ handler.py
@@ -1,7 +1,11 @@
 import random
 from textblob import TextBlob
 import nltk
-from boto.s3.connection import S3Connection
+import boto3
+import datetime
+import uuid
+from boto3.dynamodb.conditions import Key, Attr
+from decimal import *
 
 def parseInt(value):
     try:
@@ -9,84 +13,124 @@ def parseInt(value):
     except ValueError:
         return 100
 
+def connect_to_db():
+    dynamodb = boto3.resource('dynamodb', region_name='us-west-1')
+    table = dynamodb.Table('dumpling')
+    return table
+
+def get_uid(event):
+    return event['session']['user']['userId']
+
+def get_uuid():
+    return str(uuid.uuid4())
+
+def get_timestamp(timeframe=0):
+    return (datetime.datetime.now() - datetime.timedelta(days=timeframe)).isoformat()
+
+def get_polarity(textInput):
+    blob = TextBlob(textInput)
+    pol = 0
+    for sentence in blob.sentences:
+        pol += sentence.sentiment.polarity
+    return pol
+
+def classify_sentiment(pol):
+    day_type = 'bad'
+    if pol > 0:
+        day_type = 'good'
+    return day_type
+
+def list_choices():
+    list_choices = ['Go out and watch a movie. The greatest showman is in theatres.',
+    'Go talk to a friend or family',
+    'Go out and watch a movie. Paddington two is in theatres and has 100 percentage on rotten tomatoes.',
+    'I think you should watch the tv series Big bang theory. Its hilarious. You will love it.',
+    'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby0.mp3"/>',
+    'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby1.mp3"/>',
+    'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby2.mp3"/>'
+    ]
+    return list_choices
+
+def generate_text(textInput, label, dayOrWeek='day'):
+    if dayOrWeek == 'day':
+        text = 'I heard ' + textInput + '? If so, I think you had a ' + label + ' day. '
+    else:
+        text = 'You had a ' + label + ' week. '
+    if label == 'good':
+        text += 'Take rest and keep up this good spirit.'
+    else:
+        text += random.choice(list_choices())
+    return text
+
+def get_past_results(table, uid, timeframe=7):
+    sentiments = []
+    dt1 = get_timestamp()
+    dt2 = get_timestamp(timeframe)
+    filtering_exp = Key('uid').eq(uid) and Key('timestamp').between(dt2, dt1)
+    response = table.scan(FilterExpression=filtering_exp)
+    for item in response['Items']:
+        sentiments.append(float(item['sentiment']))
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(
+            FilterExpression=filtering_exp,
+            ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+        for item in response['items']:
+            sentiments.append(float(item['sentiment']))
+    return sentiments
+
+def process_help():
+    return build_response('You can describe how your day was in a sentence or few.', False)
+
+def process_stop():
+    return build_response('Ok. Closing the skill.')
+
+def process_day(textInput, uid, table, event):
+    textInput = event['request']['intent']['slots']['TextInput']
+    if 'value' in textInput:
+        textInput = str(textInput['value'])
+        if textInput.lower() == 'help':
+            return process_help()
+        elif textInput.lower() == 'stop' or textInput.lower() == 'cancel':
+            return process_stop()
+        pol = get_polarity(textInput)
+        day_type = classify_sentiment(pol)
+        text = generate_text(textInput, day_type)
+        Item = {
+                    'uuid': get_uuid(),
+                    'uid': uid,
+                    'text': textInput,
+                    'sentiment': Decimal(str(pol)),
+                    'label': day_type,
+                    'timestamp': get_timestamp()
+        }
+        table.put_item(Item=Item)
+        return build_response(text)
+    else:
+        return build_response('I could not hear anything', False)
+
+def process_week(uid, table):
+    past_results = get_past_results(table, uid)
+    if len(past_results) == 0:
+        return build_response("As of now, there are no entries recorded.")
+    avg_pol = sum(past_results)/len(past_results)
+    week_type = classify_sentiment(avg_pol)
+    text = generate_text('', week_type, 'week')
+    return build_response(text)
 
 def dumpler(event, context):
     print(event)
+    table = connect_to_db()
+    uid = get_uid(event)
     intent = event['request']['type']
     if intent == u'LaunchRequest':
         return build_response("Hi there. How was you day?", False)
     textInput = None
     intentName = event['request']['intent']['name']
     if intentName == 'DiaryInput':
-        conn = S3Connection('aws access key', 'aws secret key')
-        bucket = conn.get_bucket('hackazdailydiary')
-        key = bucket.get_key('log.txt')
-        key.get_contents_to_filename('/tmp/log.txt')
-        textInput = event['request']['intent']['slots']['TextInput']
-        if 'value' in textInput:
-            textInput = str(textInput['value'])
-            blob = TextBlob(textInput)
-            pol = 0
-            for sentence in blob.sentences:
-                pol += sentence.sentiment.polarity
-            day_type = 'rough'
-            if pol > 0:
-                day_type = 'good'
-            with open('/tmp/log.txt') as f:
-                cont = f.read().strip()
-            if cont:
-                pols = cont.split(',')
-                pols.append(str(pol))
-                if len(pols) > 7:
-                    del pols[0]
-                cont = ','.join(pols)
-            else:
-                cont = str(pol)
-            print(cont)
-            key = bucket.new_key('log.txt')
-            key.set_contents_from_string(cont)
-            key.set_acl('public-read')
-            text = 'I heard ' + textInput + '? If so, I think you had a ' + day_type + 'day. '
-            if day_type == 'good':
-                text += 'Take rest and keep up this good spirit.'
-            else:
-                text += random.choice(['Go out and watch a movie. The greatest showman is in theatres.',
-                'Go talk to a friend or family',
-                'Go out and watch a movie. Paddington two is in theatres and has 100 percentage on rotten tomatoes.',
-                'I think you should watch the tv series Big bang theory. Its hilarious. You will love it.',
-                'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby0.mp3"/>',
-                'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby1.mp3"/>',
-                'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby2.mp3"/>'
-                ])
-                # text += 'Heres a cute baby voice for you <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby0.mp3"/>'
-            return build_response(text)
+        return process_day(textInput, uid, table, event)
     else:
-        conn = S3Connection('AKIAJP5VDW25FH3WTBOA', 'jeivL76cKkPnN2MZGx0HlrPqPqaRCTGcLkzqay5q')
-        bucket = conn.get_bucket('hackazdailydiary')
-        key = bucket.get_key('log.txt')
-        key.get_contents_to_filename('/tmp/log.txt')
-        with open('/tmp/log.txt') as f:
-            cont = f.read().strip()
-        week_type = 'good'
-        if cont:
-            past_results = [float(x) for x in cont.split(',')]
-            avg = sum(past_results)/len(past_results)
-            if avg < 0:
-                week_type = 'rough'
-        text = 'You had a ' + week_type + ' week. Take a look at the app for your mood chart. '
-        if week_type == 'good':
-            text += 'Take rest and keep up this good spirit.'
-        else:
-            text += random.choice(['Go out and watch a movie. The greatest showman is in theatres.',
-            'Go talk to a friend or family',
-            'Go out and watch a movie. Paddington two is in theatres and has 100 percentage on rotten tomatoes.',
-            'I think you should watch the tv series Big bang theory. Its hilarious. You will love it.',
-            'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby0.mp3"/>',
-            'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby1.mp3"/>',
-            'Cheer up <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby2.mp3"/>'
-            ])
-            # text += 'Heres a cute baby voice for you. <audio src="https://s3.amazonaws.com/mp3filesmanjit/baby0.mp3"/>'
-        return build_response(text)
+        return process_week(uid, table)
 
     return build_response('Sorry. I could not get that')
 
